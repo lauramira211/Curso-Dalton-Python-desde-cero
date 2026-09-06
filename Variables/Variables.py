a918aff6eae14c40532b882ac3b4008c8d301532
lauramira211 <lauramira211@gmail.com>
2026-09-06 18:07:31 -0500

    Add files via upload

---
+++ Variables/Variables.py
@@ -0,0 +1,32 @@
+#Con +
+Nombre ="Lucas"
+Bienvenida= f"Hola {Nombre} ¿Como estas?"
+print (Bienvenida)
+
+# 2 opccion spring 
+Nombre_Completo ="Lucas"
+Bienvenida = f"Hola{Nombre_Completo} Como estas?"
+print (Bienvenida)
+
+#Confspring
+Nombre="Lucas"
+Bienvenida=" Hola " + (Nombre) + "¿Como estas?"
+print(Bienvenida)
+
+
+
+#Operadores de pertenencia(in/not in)
+Print=("Lucas" in Bienvenida)#True
+Print=("Lucas"not in Bienvenida)#False
+
+
+Mensaje_Completo="Tengo 20 años"
+
+
+
+
+
+
+
+
+
